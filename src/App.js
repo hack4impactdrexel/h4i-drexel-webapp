c9af4b8ab7a45d96267f507313ac72f7a9327a36
Sherry Lu <shmerryl@hotmail.com>
2022-07-22 12:57:00 -0400

    added blank header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./Components/Header"
 import HomePage from "./Components/HomePage";
 
 function App() {
   return (
     <header>
       <div>
+        <Header />
         <HomePage />
       </div>
     </header>
